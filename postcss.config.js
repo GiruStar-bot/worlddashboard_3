90eb3284f10b280b12f0626b6563a036ccdc1928
SOICHIRO <a5402s2004@gmail.com>
2026-02-12 13:05:09 +0900

    Update postcss.config.js

--- postcss.config.js
+++ postcss.config.js
@@ -1,11 +1,9 @@
 // PostCSS configuration file used by Vite.  It simply declares the
-// Tailwind CSS and Autoprefixer plugins.  Tailwind will inject its
-// utilities and component classes at build time while Autoprefixer will
-// ensure vendor prefixes are applied automatically where necessary.
+// Tailwind CSS and Autoprefixer plugins.
 
 module.exports = {
   plugins: {
-    tailwindcss: {},
+    '@tailwindcss/postcss': {},
     autoprefixer: {},
   },
 };
